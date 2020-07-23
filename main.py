ea15ee7627e975e13f036671afd295bfe56b2783
Stephen Yang <stepheny755@gmail.com>
2020-07-23 16:36:18 -0700

    Update main.py
    removed duplicate function

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 import cv2, numpy as np
 from PIL import Image,ExifTags
-import Process
-
-def save_frames(video_name,path):
-    vidcap = cv2.VideoCapture(path+video_name)
-    bgs = cv2.createBackgroundSubtractorMOG()
-    success,image = vidcap.read()
-    frame = 0
-    while success:
-        image = rotate_image(image,-90)
-        image = cv2.resize(im,(1080,1920))
-        cv2.imwrite(path+("frame%d.jpg"%frame),rotated_img)
-        success,image = vidcap.read()
-        frame+=1
-        print("Saved frame "+str(frame))
-
-
+import image_utils
 
 if(__name__=="__init__"):
     img = cv2.imread('F:\res\photos3_20200710_224834.jpg')
