2fa5964653a465d16c606d769edf97f31fe074a3
Stephen Yang <stepheny755@gmail.com>
2020-07-26 09:51:26 -0700

    Update reconstruction.py
    merged changes

--- reconstruction.py
+++ reconstruction.py
@@ -6,20 +6,14 @@ import os,cv2
 class reconstruction():
 
     def __init__(self):
-<<<<<<< Updated upstream
         self.width,self.height = pyautogui.size()
-
-    def get_width(self): return self.width
-    def get_height(self): return self.height
-=======
-        #self.max_width,self.max_height = pyautogui.size()
         self.utils = image_utils()
         img = self.utils.print_screen((100,10,400,700))
         cv2.imwrite("test.png",img)
-    #def move
+
+    def get_width(self): return self.width
+    def get_height(self): return self.height
 
 
 if(__name__=="__main__"):
-    #print(pyautogui.size())
-    reconstruction()
->>>>>>> Stashed changes
+    r = reconstruction()
