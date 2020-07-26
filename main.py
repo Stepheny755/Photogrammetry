eb75b556da499aa512d1833f291fd626e92a84f5
Stephen Yang <stepheny755@gmail.com>
2020-07-25 23:19:57 -0700

    test

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import cv2, numpy as np
 from PIL import Image,ExifTags
-import image_utils
+from process import Process
+import skimage.exposure as skie
 
-if(__name__=="__init__"):
-    img = cv2.imread('F:\res\photos3_20200710_224834.jpg')
+def save_frames(video_name,path):
+    vidcap = cv2.VideoCapture(path+video_name)
     bgs = cv2.createBackgroundSubtractorMOG()
+    success,image = vidcap.read()
+    frame = 0
+    while success:
+        image = rotate_image(image,270)
+        cv2.imwrite(path+("frame%d.jpg"%frame),rotated_img)
+        success,image = vidcap.read()
+        frame+=1
+        print("Saved frame "+str(frame))
+
+def load_images(path):
+    files = list([os.path.join(folder, f) for f in os.listdir(folder)])
+
+
+if(__name__=="__main__"):
+    #path  = ".\data\photos1"
+
+
+
+
+'''
+    print("here")
+    img = cv2.imread("1.jpg")
+    print("image read")
+    print(img)
+    bgs = cv2.createBackgroundSubtractorMOG2()
     fgmask = bgs.apply(img)
-    cv2.imwrite("test",fgmask)
+    cv2.imwrite("test.jpg",fgmask)
+    print("image saved")
+'''
--- reconstruction.py
+++ reconstruction.py
@@ -2,8 +2,8 @@ import image_utils,pyautogui
 
 class reconstruction():
 
-    def __init__():
+    def __init__(self):
+        self.width,self.height = pyautogui.size()
 
-
-
-    def
+    def get_width(self): return self.width
+    def get_height(self): return self.height
